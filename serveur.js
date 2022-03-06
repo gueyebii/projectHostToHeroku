adfd433b1d76669bb66d9eaaf7ab82089ac507c5
ndiawar gueye <ndiawar888@gmail.com>
2022-03-06 02:50:02 +0000

    parametrage pour deploiement de l'application sur heroku

---
+++ serveur.js
@@ -0,0 +1,12 @@
+const express =  require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/angular-project2'));
+
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/angular-project2/index.html'))
+})
+
+app.listen(process.env.PORT);
