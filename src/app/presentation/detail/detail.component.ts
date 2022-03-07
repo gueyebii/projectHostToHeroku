7c51b1537cebb49ed0d2a56d18f5011a1736c858
ndiawar gueye <ndiawar888@gmail.com>
2022-03-07 21:18:05 +0000

    new commit

---
+++ src/app/presentation/detail/detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Personne } from 'src/app/model/personne';
+import { TestService } from '../test/test.service';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.css']
+})
+export class DetailComponent implements OnInit {
+
+  personne: Personne;
+  constructor(
+    private testservice: TestService,
+    private activatedRouter: ActivatedRoute
+  ) {
+    this.personne = new Personne;
+   }
+
+  ngOnInit(): void {
+    this.activatedRouter.params.subscribe(
+      (params) => {
+        this.personne = this.testservice.getElelementById(params['id'])
+      }
+    )
+  }
+
+
+}
